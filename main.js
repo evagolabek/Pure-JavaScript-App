35e979abc428a1743d80491eb2559dcab833591e
evagolabek <golabek.eva@gmail.com>
2018-07-13 17:23:23 +0200

    insert issue to the local storage

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueId = chance.guid();
+  var issueStatus = 'Open';
+
+  var issue - {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+
+  //inserting the object in local storage, first checking if there is something  in local localStorage
+  if(localStorage.getItem('issues') == null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issue));
+  }
+}
+
+
 function fetchIssues() {
   var issues = JSON.parse(localStorage.getItem('issues'));
   var issuesList = document.getElementById('issuesList');
@@ -19,7 +49,7 @@ function fetchIssues() {
                              '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
                              '<a href="#" onClick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>'+
                              '<a href="#" onClick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'
-                             '</div>'; 
+                             '</div>';
 
   }
 }
